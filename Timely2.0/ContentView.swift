58cd65d5ac624298a41887f2b4484f084620a97b
Riya Batla <riyabatla72@gmail.com>
2024-01-27 22:08:47 +0530

    Initial Commit

---
+++ Timely2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timely2.0
+//
+//  Created by Riya Batla on 27/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
